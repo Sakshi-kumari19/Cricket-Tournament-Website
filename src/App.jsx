3b8a3eb1f1d27f5527c042dc87350a2e1576153a
SAKSHI KUMARI <122092273+Sakshi-kumari19@users.noreply.github.com>
2024-11-27 11:24:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Footer from './components/Footer'

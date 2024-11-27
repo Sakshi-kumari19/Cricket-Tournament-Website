b50fe2236ac70c161e6ce9fc19c8fa23279a1dfb
SAKSHI KUMARI <sakshi.kumar19902@gmail.com>
2024-11-27 10:50:29 +0530

    Initial commit

--- src/components/Tournament.jsx
+++ src/components/Tournament.jsx
@@ -40,13 +40,13 @@ function Tournament() {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 my-10 ' >
             {
                 itemcard.map(({id,logo,name})=>(
-                    <div className='md:w-[400px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 duration-300 bg-white ' key={id}>
+                    <div className='md:w-[400px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 duration-300 bg-orange-300 ' key={id}>
                         <img src={logo} className='w-[400px] h-[150px] p-1 rounded-2xl border-[2px] ' />
                         <div>
                             <div className='px-2 font-semibold mb-2 text-center text-xl'>{name}</div>
                             <p className='px-2 text-gray-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                         </div>
-                        <div className=' space-x-4 px-6 py-4 items-center flex justify-center '>
+                        <div className=' space-x-4 px-6 py-4 items-center flex justify-center  '>
                             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-8 py-2 rounded shadow-xl '>Know More</button>
                         </div>
                     </div>
